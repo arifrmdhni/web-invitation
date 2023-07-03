8a6f67c793f1b0c01183967f26afa27de8f58e01
Arif Ramadhani <62203067+arifrmdhni@users.noreply.github.com>
2023-07-03 13:20:43 +0700

    Update CardHeader.js
    Change card header logo

--- src/Components/CardHeader.js
+++ src/Components/CardHeader.js
@@ -7,11 +7,9 @@ const CardHeader = () => {
   return (
     <div className="card-header" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="1000">
       <div className="logo-wrapper">
-        <img src={logoAsean}></img>
-        <img src={logoKBUMN}></img>
+        <img src={logoBUMN}></img>
       </div>
       <div className="logo-wrapper">
-        <img src={logoBUMN}></img>
         <img src={logoBTN}></img>
       </div>
     </div>
