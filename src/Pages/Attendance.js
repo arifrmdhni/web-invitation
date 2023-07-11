06aba4049b512803ac36e34baad8d4e5a5426b8b
Arif Ramadhani <62203067+arifrmdhni@users.noreply.github.com>
2023-07-11 16:37:00 +0700

    Update Attendance.js

--- src/Pages/Attendance.js
+++ src/Pages/Attendance.js
@@ -145,7 +145,7 @@ const Attendance = () => {
               <div className="select-wrapper">
                 <label className="" htmlFor="jenis">Peserta Online/Offline:</label>
                 <select htmlFor="jenis" onChange={(e) => {setJenisPeserta(e.target.value); handleInputType()}} name="jenis_peserta" value={jenis_peserta} required>
-                  <option htmlFor="jenis" disabled selected>-- Pilih Opsi Online/Offline --</option>
+                  <option htmlFor="jenis" readOnly>-- Pilih Opsi Online/Offline --</option>
                   <option htmlFor="jenis" value="online">Online</option>
                   <option htmlFor="jenis" value="offline">Offline</option>
                 </select>
@@ -153,7 +153,7 @@ const Attendance = () => {
               <div className="select-wrapper">
                 <label className="" htmlFor="sesi">Sesi Kehadiran:</label>
                 <select htmlFor="sesi" onChange={(e) => {setSesiAttendance(e.target.value); handleInputType()}} name="sesi_attendance" value={sesi_attendance} required>
-                  <option htmlFor="sesi" disabled selected>-- Pilih Opsi Sesi --</option>
+                  <option htmlFor="sesi" readOnly>-- Pilih Opsi Sesi --</option>
                   <option htmlFor="sesi" value="pagi">Pagi</option>
                   <option htmlFor="sesi" value="siang">Siang</option>
                   <option htmlFor="sesi" value="malam">Malam</option>
