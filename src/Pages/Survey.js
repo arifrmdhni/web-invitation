e8e4a66057cc5e766e0d825297dc80b5a4f03fd7
Arif Ramadhani <62203067+arifrmdhni@users.noreply.github.com>
2023-07-12 19:06:30 +0700

    Update Survey.js

--- src/Pages/Survey.js
+++ src/Pages/Survey.js
@@ -257,7 +257,7 @@ const Questionnaire = () => {
                   <ul>
                     <li>
                       <input onChange={(e) => {setAnswer7(e.target.value); }} type="radio" value="Ya" name="answer_7" id="otherAnswer7" required/> Ya
-                      <input onChange={(e) => {setAnswer7(e.target.value); }} type="text" className="input-answer" name="answer_7" id="inputOtherAnswer" placeholder="seberapa sering?" required/>
+                      <input onChange={(e) => {setAnswer7(e.target.value); }} type="text" className="input-answer" name="answer_7" id="inputOtherAnswer" placeholder="seberapa sering?"/>
                     </li>
                     <li>
                       <input onChange={(e) => {setAnswer7(e.target.value); }} type="radio" value="Tidak" name="answer_7" id="answer7" /> Tidak
