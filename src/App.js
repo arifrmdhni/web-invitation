209c0b6098e1ec928a0dd767e134551b64bafd8b
Arif Ramadhani <62203067+arifrmdhni@users.noreply.github.com>
2023-07-13 16:03:06 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import audioBacksound from "./assets/backsound.mp3"
 import AOS from "aos"
 import "aos/dist/aos.css"
 import MorningAttendance from "./Pages/MorningAttendance";
+import Copyright from "./Components/Copyright";
 
 function App() {
   const [playing, toggleSound] = UseAudio(audioBacksound)
@@ -63,6 +64,7 @@ function App() {
           </button>
         </div>
       </Router>
+      <Copyright />
     </div>
     </>
   );
