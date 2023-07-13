9210d260c3304eec8ef38496ae76d3d6630f9959
Arif Ramadhani <62203067+arifrmdhni@users.noreply.github.com>
2023-07-13 11:19:12 +0700

    Update NotFound.js

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,7 +1,18 @@
+import CardHeader from "../Components/CardHeader"
+import NavigationBar from "../Components/NavigationBar"
+
 const NotFound = () => {
   return (
     <>
-    
+    <div className="card custom-card">
+      <CardHeader />
+      <div className="card-body custom-card-body">
+        <div className="card-invite">
+          <p className="invite-text invite-text-title" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1500" data-aos-duration="1000" data-aos-offset="0"> 404 NotFound! </p>
+        </div>
+      </div>
+    </div>
+    <NavigationBar />
     </>
   )
 }
